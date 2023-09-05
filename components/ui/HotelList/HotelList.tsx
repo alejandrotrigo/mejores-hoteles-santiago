224e28fed1f9ad1fafa15a4b2f2b162f3a02514c
alejandrodeus <121810523+alejandrodeus@users.noreply.github.com>
2023-09-05 19:57:46 +0200

    added pics for hotels

--- components/ui/HotelList/HotelList.tsx
+++ components/ui/HotelList/HotelList.tsx
@@ -1,30 +1,103 @@
 import React from 'react'
-import { HotelListContainer, HotelListItemContainer, HotelListWrapper } from './hotellist.styles'
+import { HotelListContainer, HotelListItemButton, HotelListItemContainer, HotelListItemContent, HotelListItemDescription, HotelListItemImage, HotelListItemPrice, HotelListItemPriceContainer, HotelListItemTitle, HotelListWrapper } from './hotellist.styles'
 
 export const HotelList = () => {
 	return (
 		<HotelListContainer>
 			<HotelListWrapper>
 				<HotelListItemContainer>
-					Hotel1
+					<HotelListItemImage src='/hoteles/casa-beatnik.jpg'/>
+					<HotelListItemContent>
+						<HotelListItemTitle>Casa Beatnik</HotelListItemTitle>
+						<HotelListItemDescription>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae at consequuntur animi distinctio ab soluta tempora natus culpa aperiam cumque, vitae ipsa hic molestias suscipit? Numquam necessitatibus excepturi quod. Doloribus.
+						</HotelListItemDescription>
+						<HotelListItemPriceContainer>
+							<HotelListItemButton>Reservar</HotelListItemButton>
+							<HotelListItemPrice>20€</HotelListItemPrice>
+						</HotelListItemPriceContainer>
+					</HotelListItemContent>
 				</HotelListItemContainer>
 				<HotelListItemContainer>
-					Hotel1
+					<HotelListItemImage src='/hoteles/nh-collection.webp'/>
+					<HotelListItemContent>
+						<HotelListItemTitle>NH Collection Santiago de Compostela</HotelListItemTitle>
+						<HotelListItemDescription>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae at consequuntur animi distinctio ab soluta tempora natus culpa aperiam cumque, vitae ipsa hic molestias suscipit? Numquam necessitatibus excepturi quod. Doloribus.
+						</HotelListItemDescription>
+						<HotelListItemPriceContainer>
+							<HotelListItemButton>Reservar</HotelListItemButton>
+							<HotelListItemPrice>20€</HotelListItemPrice>
+						</HotelListItemPriceContainer>
+					</HotelListItemContent>
+	
 				</HotelListItemContainer>
 				<HotelListItemContainer>
-					Hotel1
+					<HotelListItemImage src='/hoteles/reis-catolicos.jpg'/>
+					<HotelListItemContent>
+						<HotelListItemTitle>Parador de Santiago - Hostal Reis Católicos</HotelListItemTitle>
+						<HotelListItemDescription>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae at consequuntur animi distinctio ab soluta tempora natus culpa aperiam cumque, vitae ipsa hic molestias suscipit? Numquam necessitatibus excepturi quod. Doloribus.
+						</HotelListItemDescription>
+						<HotelListItemPriceContainer>
+							<HotelListItemButton>Reservar</HotelListItemButton>
+							<HotelListItemPrice>20€</HotelListItemPrice>
+						</HotelListItemPriceContainer>
+					</HotelListItemContent>
 				</HotelListItemContainer>
 				<HotelListItemContainer>
-					Hotel1
+					<HotelListItemImage src='/hoteles/quinta-auga.webp'/>
+					<HotelListItemContent>
+						<HotelListItemTitle>A Quinta da Auga </HotelListItemTitle>
+						<HotelListItemDescription>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae at consequuntur animi distinctio ab soluta tempora natus culpa aperiam cumque, vitae ipsa hic molestias suscipit? Numquam necessitatibus excepturi quod. Doloribus.
+						</HotelListItemDescription>
+						<HotelListItemPriceContainer>
+							<HotelListItemButton>Reservar</HotelListItemButton>
+							<HotelListItemPrice>20€</HotelListItemPrice>
+						</HotelListItemPriceContainer>
+					</HotelListItemContent>
+	
 				</HotelListItemContainer>
 				<HotelListItemContainer>
-					Hotel1
+					<HotelListItemImage src='/hoteles/palacio-carmen.webp'/>
+					<HotelListItemContent>
+						<HotelListItemTitle>Hotel Palacio del Carmen, Autograph Collection</HotelListItemTitle>
+						<HotelListItemDescription>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae at consequuntur animi distinctio ab soluta tempora natus culpa aperiam cumque, vitae ipsa hic molestias suscipit? Numquam necessitatibus excepturi quod. Doloribus.
+						</HotelListItemDescription>
+						<HotelListItemPriceContainer>
+							<HotelListItemButton>Reservar</HotelListItemButton>
+							<HotelListItemPrice>20€</HotelListItemPrice>
+						</HotelListItemPriceContainer>
+					</HotelListItemContent>
 				</HotelListItemContainer>
 				<HotelListItemContainer>
-					Hotel1
+					<HotelListItemImage src='/hoteles/araguaney.webp'/>
+					<HotelListItemContent>
+						<HotelListItemTitle>Eurostars Araguaney</HotelListItemTitle>
+						<HotelListItemDescription>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae at consequuntur animi distinctio ab soluta tempora natus culpa aperiam cumque, vitae ipsa hic molestias suscipit? Numquam necessitatibus excepturi quod. Doloribus.
+						</HotelListItemDescription>
+						<HotelListItemPriceContainer>
+							<HotelListItemButton>Reservar</HotelListItemButton>
+							<HotelListItemPrice>20€</HotelListItemPrice>
+						</HotelListItemPriceContainer>
+					</HotelListItemContent>
+	
 				</HotelListItemContainer>
 				<HotelListItemContainer>
-					Hotel1
+					<HotelListItemImage src='/hoteles/san-lazaro.jpg'/>
+					<HotelListItemContent>
+						<HotelListItemTitle>Eurostarts San Lázaro</HotelListItemTitle>
+						<HotelListItemDescription>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae at consequuntur animi distinctio ab soluta tempora natus culpa aperiam cumque, vitae ipsa hic molestias suscipit? Numquam necessitatibus excepturi quod. Doloribus.
+						</HotelListItemDescription>
+						<HotelListItemPriceContainer>
+							<HotelListItemButton>Reservar</HotelListItemButton>
+							<HotelListItemPrice>20€</HotelListItemPrice>
+						</HotelListItemPriceContainer>
+					</HotelListItemContent>
 				</HotelListItemContainer>
 			</HotelListWrapper>
 		</HotelListContainer>
--- components/ui/HotelList/hotellist.styles.tsx
+++ components/ui/HotelList/hotellist.styles.tsx
@@ -13,7 +13,7 @@ export const HotelListWrapper = styled('div')`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	justify-content: center;
+	justify-content: flex-start;
 	max-width: 1200px;
 	height: 100%;
 	gap: 1rem;
@@ -21,10 +21,92 @@ export const HotelListWrapper = styled('div')`
 
 export const HotelListItemContainer = styled('div')`
 	display: flex;
-	align-items: center;
+	align-items: flex-start;
 	justify-content: space-between;
 	width: 100%;
-	height: 200px;
+	height: 250px;
+	padding: 1rem 0.5rem;
 
 	border: 1px solid #e7e7e7;
+	border-radius: 3px;
+
+	&:hover {
+		cursor: pointer;
+	}
+`;
+
+export const HotelListItemImage = styled.img`
+	width: 200px;
+	height: 200px;
+	background-size: cover;
+	background-repeat: none;
+
+	border-radius: 8px;
+`;
+
+export const HotelListItemContent = styled('div')`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	justify-content: flex-start;
+	width: 100%;
+	height: 100%;
+
+	padding: 1rem;
+`;
+
+export const HotelListItemTitle = styled('h3')`
+	font-size: 1.5rem;
+	font-weight: 600;
+	margin: 0;
+`;
+
+export const HotelListItemDescription = styled('p')`
+	font-size: 1rem;
+	font-weight: 400;
+`;
+
+export const HotelListItemPriceContainer = styled('div')`
+	display: flex;
+	align-items: center;
+	justify-content: flex-start;
+	width: 100%;
+
+`;
+
+export const HotelListItemPrice = styled('p')`
+	font-size: 1.5rem;
+	font-weight: 600;
+	margin: 0;
+	margin-left: auto;
+`;
+
+export const HotelListItemButton = styled('button')`
+	width: 12rem;
+	height: 2.5rem;
+	margin: 0;
+	border: 1px solid #ddd;
+	background-color: #006ce4;
+	color: white;
+
+	font-weight: 400;
+	font-family: Bebas Neue, serif;
+	letter-spacing: 0.15rem;
+	font-size: 1.05rem;
+	padding: 0.5rem 0.5rem;
+
+	cursor: pointer;
+
+	transition: all 0.3s ease-in-out;
+
+	&:hover {
+		background-color: #035fdf;
+		color: #fff;
+	}
+
+	&:hover:disabled {
+		background-color: rgb(34, 193, 195, 0.35);
+		box-shadow: none;
+		cursor: not-allowed;
+	}
 `;
