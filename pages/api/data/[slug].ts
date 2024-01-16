fc2e417ca2e8544e69e685456b8b979e2eccaf7e
alejandrodeus <121810523+alejandrodeus@users.noreply.github.com>
2024-01-16 12:12:49 +0100

    Add axios for API request in HotelPage component

--- pages/api/data/[slug].ts
+++ pages/api/data/[slug].ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
 	const { slug } = req.query;
-	const data = JSON.parse(fs.readFileSync(path.resolve(`public/hoteles/${slug}/data.json`), 'utf-8'));
+	const data = JSON.parse(fs.readFileSync(path.resolve(`/hoteles/${slug}/data.json`), 'utf-8'));
 
   res.status(200).json(data);
 }
