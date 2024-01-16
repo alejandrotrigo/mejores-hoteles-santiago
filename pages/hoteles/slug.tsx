54ada4d797cbd21b93ebff7bb70f82638e56fc6c
alejandrodeus <121810523+alejandrodeus@users.noreply.github.com>
2024-01-16 16:03:38 +0100

    Add new hotel pages and update API handler

--- pages/hoteles/slug.tsx
+++ pages/hoteles/slug.tsx
@@ -12,14 +12,11 @@ interface Props {
 }
 
 const HotelPage: FC<Props> = ({ data }) => {
-	const router = useRouter();
-	const handleClick = () => {
-		router.replace(data.booking);
-	}
+	
 
 		return (
 		<MainLayout title={'Mejores hoteles de Santiago de Compostela'} pageDescription={'Ven a conocer Santiago de Compostela de la mano de los mejores hoteles, el lujo y experiencias auténticas '} imageFullUrl="webinas.png">
-			<MainTitle title={data.title} subtitle={data.subtitle} button="ver en booking" handleClick={handleClick}/>
+			<MainTitle title={data.title} subtitle={data.subtitle} />
 			<Collage 
 				url1={`/hoteles/${data.slug}/images/image1.jpg`}
 				url2={`/hoteles/${data.slug}/images/image2.jpg`}
@@ -33,16 +30,16 @@ const HotelPage: FC<Props> = ({ data }) => {
 	)
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+/*export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const { slug } = params as { slug: string };
   
-  /*const { data } = await axios.get(`${process.env.SITE_URL}/api/data/${slug}`);*/
+  /*const { data } = await axios.get(`${process.env.SITE_URL}/api/data/${slug}`);
 
   return {
     props: {
       data: {} || null,
     }
   }
-}
+}*/
 
 export default HotelPage
